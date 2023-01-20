2a388e40c9c57b93e779e788ce90aaea5a163071
Matt Garber <matthew.garber@childrens.harvard.edu>
2023-01-20 14:49:06 -0500

    import pytest

--- tests/integration/test_api_gateway.py
+++ tests/integration/test_api_gateway.py
@@ -5,6 +5,7 @@ import json
 from urllib import request
 import boto3
 import requests
+import pytest
 
 """
 Make sure env variable AWS_SAM_STACK_NAME exists with the name of the stack we are going to test. 
