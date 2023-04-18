014d33459f3c645b189b8e0cb157c83ad6c809c9
matt garber <Matthew.Garber@childrens.harvard.edu>
2023-04-18 10:13:35 -0400

    Study period API (#73)
    * Study period API
    
    * corrected unit test imports
    
    * cleanup, updated OpenAPI spec

---
+++ tests/dashboard/test_get_study_periods.py
@@ -0,0 +1,39 @@
+import boto3
+import json
+import os
+
+import pytest
+from datetime import datetime, timezone
+from unittest import mock
+
+from src.handlers.shared.enums import BucketPath
+from src.handlers.shared.functions import read_metadata, write_metadata
+from src.handlers.dashboard.get_study_periods import study_periods_handler
+from tests.utils import get_mock_study_metadata
+
+
+@pytest.mark.parametrize(
+    "params,status,expected",
+    [
+        (None, 200, get_mock_study_metadata()),
+        (
+            {"site": "general_hospital"},
+            200,
+            get_mock_study_metadata()["general_hospital"],
+        ),
+        (
+            {"site": "general_hospital", "study": "covid"},
+            200,
+            get_mock_study_metadata()["general_hospital"]["covid"],
+        ),
+        ({"site": "chicago_hope", "study": "covid"}, 500, None),
+        ({"site": "general_hospital", "study": "flu"}, 500, None),
+    ],
+)
+def test_get_study_periods(mock_bucket, params, status, expected):
+    event = {"pathParameters": params}
+
+    res = study_periods_handler(event, {})
+    assert res["statusCode"] == status
+    if status == 200:
+        assert json.loads(res["body"]) == expected
