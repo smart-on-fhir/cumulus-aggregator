3cf38c4144b536d15993f1ec9502a817eb6f7655
Matt Garber <matthew.garber@childrens.harvard.edu>
2024-10-03 15:06:51 -0400

    coverage

--- tests/site_upload/test_fetch_upload_url.py
+++ tests/site_upload/test_fetch_upload_url.py
@@ -1,9 +1,11 @@
 import json
+from unittest import mock
 
+import botocore
 import pytest
 
-from src.handlers.shared.enums import BucketPath
-from src.handlers.site_upload.fetch_upload_url import upload_url_handler
+from src.handlers.shared import enums
+from src.handlers.site_upload import fetch_upload_url
 from tests.mock_utils import (
     EXISTING_DATA_P,
     EXISTING_SITE,
@@ -34,13 +36,24 @@ def test_fetch_upload_url(body, status, mock_bucket):
         }
     }
 
-    response = upload_url_handler({"body": json.dumps(body), "requestContext": context}, None)
+    response = fetch_upload_url.upload_url_handler(
+        {"body": json.dumps(body), "requestContext": context}, None
+    )
     assert response["statusCode"] == status
     if response["statusCode"] == 200:
         res_body = json.loads(response["body"])
         assert res_body["fields"]["key"] == (
-            f"{BucketPath.UPLOAD.value}/{body['study']}/{body['data_package']}/"
+            f"{enums.BucketPath.UPLOAD.value}/{body['study']}/{body['data_package']}/"
             f"{EXISTING_SITE}/{body['data_package_version']}/encounter.parquet"
         )
 
     assert "Access-Control-Allow-Origin" not in response["headers"]
+
+
+@mock.patch("boto3.client")
+def test_create_presigned_post_error(mock_client):
+    mock_client.return_value.generate_presigned_post.side_effect = botocore.exceptions.ClientError(
+        error_response={}, operation_name="op"
+    )
+    res = fetch_upload_url.create_presigned_post("bucket", "obj")
+    assert res["statusCode"] == 400
