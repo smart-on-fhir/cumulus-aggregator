7cd824ff3df42fd29336791984e86532a70dd247
matt garber <Matthew.Garber@childrens.harvard.edu>
2024-10-07 15:54:19 -0400

    Added filtering to data_packages (#123)
    * Added filtering to data_packages
    
    * invalid ID test

--- src/handlers/dashboard/get_data_packages.py
+++ src/handlers/dashboard/get_data_packages.py
@@ -10,11 +10,28 @@ from src.handlers.shared.functions import get_s3_json_as_dict, http_response
 @generic_error_handler(msg="Error retrieving data packages")
 def data_packages_handler(event, context):
     """Retrieves list of data packages from S3."""
-    del event
     del context
+    status = 200
     data_packages = get_s3_json_as_dict(
         os.environ.get("BUCKET_NAME"),
         f"{BucketPath.CACHE.value}/{JsonFilename.DATA_PACKAGES.value}.json",
     )
-    res = http_response(200, data_packages, allow_cors=True)
+    payload = data_packages
+    if event.get("queryStringParameters"):
+        filtered_packages = []
+        for package in data_packages:
+            if package["name"] == event["queryStringParameters"]["name"]:
+                filtered_packages.append(package)
+        payload = filtered_packages
+    elif event.get("pathParameters"):
+        found = None
+        for package in data_packages:
+            if event["pathParameters"]["id"] == package["id"]:
+                found = package
+        if found:
+            payload = found
+        else:
+            status = 404
+            payload = None
+    res = http_response(status, payload, allow_cors=True)
     return res
